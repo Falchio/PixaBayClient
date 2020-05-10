63403977baffe5bbc46582284930d671a5f46c54
Falchio <53504233+Falchio@users.noreply.github.com>
2020-05-10 21:59:41 +0300

    search block added and some fix
    some fix

--- app/src/main/java/ru/falchio/pixabayclient/model/Model.java
+++ app/src/main/java/ru/falchio/pixabayclient/model/Model.java
@@ -16,38 +16,24 @@ import ru.falchio.pixabayclient.json.PixaRequest;
 
 public class Model {
     private List<PixaImageUrl> pixaImages;
-
     private final String TAG = this.getClass().getSimpleName();
     private PixaRequest request;
     private PixaAnswer pixaAnswer;
     private final String API = "16246042-74f45b97e0abdd7225fc1506d";
-    private final String SEARCH_KEY_WORDS = "yellow+flowers";
-    private final String IMAGE_TYPE = "photo";
-
-
+    private Retrofit retrofit;
 
 
     public Model() {
         initRetrofit();
     }
 
-    public List<PixaImageUrl> getPixaImages() {
-        return pixaImages;
-    }
-
-    private void initRetrofit() {
-        Retrofit retrofit;
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://pixabay.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        request = retrofit.create(PixaRequest.class);
-
-        request.loadImage(API, SEARCH_KEY_WORDS, IMAGE_TYPE).enqueue(new Callback<PixaAnswer>() {
+    public List<PixaImageUrl> getPixaImages(String wordsForSearch, String imageType) {
+        wordsForSearch.replaceAll(" ","+");
+        request.loadImage(API, wordsForSearch, imageType).enqueue(new Callback<PixaAnswer>() {
             @Override
             public void onResponse(Call<PixaAnswer> call, Response<PixaAnswer> response) {
-                 pixaAnswer = response.body();
-                 pixaImages =Arrays.asList(pixaAnswer.getPixaImageUrls());
+                pixaAnswer = response.body();
+                pixaImages = Arrays.asList(pixaAnswer.getPixaImageUrls());
 
 //                Log.d(TAG, "onResponse: " + pixaAnswer.toString());
             }
@@ -57,5 +43,15 @@ public class Model {
                 Log.d(TAG, "onFailure: " + t.toString());
             }
         });
+        return pixaImages;
+    }
+
+    private void initRetrofit() {
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://pixabay.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        request = retrofit.create(PixaRequest.class);
     }
 }
